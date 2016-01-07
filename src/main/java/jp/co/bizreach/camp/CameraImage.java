c3e603b6907a38af196b23c820b8d728dbd5cca3
李均範 <kyunbum.yi@bizreach.co.jp>
2016-01-08 02:02:53 +0900

    /tmp/motionから最新のイメージファイルを取得

--- src/main/java/jp/co/bizreach/camp/CameraImage.java
+++ src/main/java/jp/co/bizreach/camp/CameraImage.java
@@ -6,6 +6,17 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Date;
+
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.comparator.LastModifiedFileComparator;
+import org.apache.commons.io.filefilter.FileFileFilter;
+
+
+import java.io.File;
 
 @Component
 public class CameraImage {
@@ -18,7 +29,34 @@ public class CameraImage {
 	 */
 	public File getLatestImage() {
 		log.info("getting image file!");
+		
+		File directory = new File("/tmp/motion");
+		// get just files, not directories
+		File[] files = directory.listFiles((FileFilter) FileFileFilter.FILE);
+
+//		System.out.println("Default order");
+//		File file = findImage(files);
+//
+//		Arrays.sort(files, LastModifiedFileComparator.LASTMODIFIED_COMPARATOR);
+//		System.out.println("\nLast Modified Ascending Order (LASTMODIFIED_COMPARATOR)");
+//		File file = findImage(files);
+
+		Arrays.sort(files, LastModifiedFileComparator.LASTMODIFIED_REVERSE);
+//		System.out.println("\nLast Modified Descending Order (LASTMODIFIED_REVERSE)");
+		File file = findImage(files);
+		
+		return file;
+	}
+	
+	public File findImage(File[] files) {
+		for (File file : files) {
+			//System.out.printf("File: %-20s Last Modified:" + new Date(file.lastModified()) + "\n", file.getName());
+			if (FilenameUtils.getExtension(file.getName()).toLowerCase() == "jpg") {
+				return file;
+			}
+		}
 		return new File("");
 	}
+
 
 }
