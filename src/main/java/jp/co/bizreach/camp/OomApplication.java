5f1a31ee9fdbdd1853a7f8e5c99629e3b1c8bd47
yu.watanabe@bizreach.co.jp <yu.watanabe@BIZ2015-MAC0005.local>
2016-01-08 00:49:32 +0900

    mvn compile exec:java で動くようにした。

--- src/main/java/jp/co/bizreach/camp/OomApplication.java
+++ src/main/java/jp/co/bizreach/camp/OomApplication.java
@@ -3,13 +3,16 @@ package jp.co.bizreach.camp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OomApplication {
 
-//	public static void main(String[] args) {
-//		SpringApplication.run(OomApplication.class, args);
-//	}
+	public static void main(String[] args) {
+		ConfigurableApplicationContext ctx = SpringApplication.run(OomApplication.class, args);
+		OmiyageOiteokimashitaMail oom = ctx.getBean(OmiyageOiteokimashitaMail.class);
+		oom.execute();
+	}
 
 }
