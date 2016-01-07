866f44081fcf930ad8401ea4543a1ac07d77f9d9
yu.watanabe@bizreach.co.jp <yu.watanabe@BIZ2015-MAC0005.local>
2016-01-07 22:56:31 +0900

    first commit

---
+++ src/main/java/jp/co/bizreach/camp/OomApplication.java
@@ -0,0 +1,14 @@
+package jp.co.bizreach.camp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class OomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OomApplication.class, args);
+	}
+}
---
+++ src/test/java/jp/co/bizreach/camp/OomApplicationTests.java
@@ -0,0 +1,16 @@
+package jp.co.bizreach.camp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = OomApplication.class)
+public class OomApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
