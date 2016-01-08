f0f99b148bd3d7175b2b7271edd255ba296adc5f
yu.watanabe@bizreach.co.jp <yu.watanabe@BIZ2015-MAC0005.local>
2016-01-08 10:16:11 +0900

    bashスクリプトがまちがっていた

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
+sudo service motion stop
 sudo rm -fr /tmp/motion/*.jpg
-sudo service mothion start
+sudo service motion start
 mvn compile exec:java -DsmtpPasswd=temporaryPassword
 sudo service mothion stop
