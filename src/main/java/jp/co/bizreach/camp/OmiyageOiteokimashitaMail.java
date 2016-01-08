3ada5d6c92fd187817ca2d056574550051a1191d
yu.watanabe@bizreach.co.jp <yu.watanabe@BIZ2015-MAC0005.local>
2016-01-08 09:41:04 +0900

    常駐プロセスとして稼働できるようにwebアプリ化した（ただし画面はない）

--- src/main/java/jp/co/bizreach/camp/OmiyageOiteokimashitaMail.java
+++ src/main/java/jp/co/bizreach/camp/OmiyageOiteokimashitaMail.java
@@ -3,6 +3,7 @@ package jp.co.bizreach.camp;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
@@ -21,6 +22,7 @@ public class OmiyageOiteokimashitaMail {
 	@Autowired
 	private SshLogic sshLogic;
 
+	@Scheduled(fixedDelay = 2000)
 	public void execute() {
 		log.info("Hello World !");
 		sshLogic.countDown();
--- src/main/java/jp/co/bizreach/camp/OomApplication.java
+++ src/main/java/jp/co/bizreach/camp/OomApplication.java
@@ -5,14 +5,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class OomApplication {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext ctx = SpringApplication.run(OomApplication.class, args);
 		OmiyageOiteokimashitaMail oom = ctx.getBean(OmiyageOiteokimashitaMail.class);
-		oom.execute();
+		//oom.execute();
 	}
 
 }
